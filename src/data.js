28df61a6d937b896b1ef59b442b3d7d8ca4f9c6c
Resham Chawra <reshamchawra18@gmail.com>
2020-06-23 13:44:58 +0530

    first comit

---
+++ src/data.js
@@ -0,0 +1,112 @@
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info:
+      "Google Pixel 3 smartphone has a OLED display. It measures 145.6 mm x 68.2 mm x 7.9 mm and weighs 148 grams. The screen has a resolution of 1080 x 2160 pixels and 439 ppi pixel density. It has an aspect ratio of 18:9 and screen-to-body ratio of 78.62 %. On camera front, the buyers get a 8 MP f/2.2, Wide Angle Primary Camera(28 mm focal length)8 MP, Wide Angle, Ultra-Wide Angle Camera and on the rear, there's an 12.2 MP camera with features like Digital Zoom, Auto Flash, Face detection, Touch to focus. It is backed by a 2915 mAh battery.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 16,
+    company: "SAMSUNG",
+    info:
+      "Samsung Galaxy S7 smartphone has a Super AMOLED display. It measures 142.4 mm x 69.6 mm x 7.9 mm and weighs 152 grams. The screen has a resolution of 1440 x 2560 pixels and 576 ppi pixel density. On camera front, the buyers get a 5 MP f/1.7, Wide Angle Primary Camera(22 mm focal length, 4.1 sensor size, 1.34µm pixel size) and on the rear, there an 12 MP camera with features like Digital Zoom, Auto Flash, Digital image stabilization, Face detection, Smile detection, Touch to focus. It is backed by a 3000 mAh battery.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC 10 - Black",
+    img: "img/product-3.png",
+    price: 8,
+    company: "htc",
+    info:
+      "HTC 10 smartphone has a S-LCD 5 display. It measures 145.9 mm x 71.9 mm x 9.0 mm and weighs 161 grams. The screen has a resolution of 1440 x 2560 pixels and 565 ppi pixel density. On camera front, the buyers get a 5 MP f/1.8 Primary Camera and on the rear, there's an 12 MP camera with features like Digital Zoom, Auto Flash, Face detection, Touch to focus. It is backed by a 3000 mAh battery. Connectivity features in the smartphone include WiFi, Bluetooth, GPS, Volte, NFC and more.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HTC evo 10 - White",
+    img: "img/product-4.png",
+    price: 18,
+    company: "htc",
+    info:
+      "HTC evo 10 White smartphone comes with a 5.5-inch touchscreen display with a resolution of 1440 pixels by 2560 pixels. HTC 10 evo is available at best price on Gadgets Now. Powered by octa-core Qualcomm Snapdragon 810 processor, HTC 10 evo comes with 3GB of RAM. The phone packs 32GB of internal storage that can be expanded up to 2000GB via a microSD card. The phone packs a 16-megapixel primary camera on the rear and an 8-megapixel secondary camera.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "HTC Desire 626s",
+    img: "img/product-5.png",
+    price: 24,
+    company: "htc",
+    info:
+      "HTC Desire 626s smartphone comes with a 5.00-inch touchscreen display with a resolution of 720x1280 pixels. HTC Desire 626s is powered by a 1.1GHz quad-core Qualcomm Snapdragon 210 processor. It comes with 1GB of RAM. The HTC Desire 626s runs Android 5.1 and is powered by a 2000mAh removable battery.As far as the cameras are concerned, the HTC Desire 626s on the rear packs 8-megapixel camera. It sports a 2-megapixel camera on the front for selfies.The HTC Desire 626s runs Sense 7 based on Android 5.1.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Iphone",
+    img: "img/product-6.png",
+    price: 17,
+    company: "apple",
+    info:
+      "The phone comes with a 4.00-inch touchscreen display with a resolution of 640x1136 pixels at a pixel density of 326 pixels per inch (ppi) and an aspect ratio of 16:9. Apple iPhone 5s is powered by a 1.3GHz dual-core Apple A7 processor. It comes with 1GB of RAM. The Apple iPhone 5s runs iOS 7 and is powered by a 1570mAh non-removable battery.As far as the cameras are concerned, the Apple iPhone 5s on the rear packs an 8-megapixel camera with an f/2.2 aperture and a pixel size of 1.5-micron. The rear camera setup has autofocus.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 7",
+    img: "img/product-7.png",
+    price: 30,
+    company: "apple",
+    info:
+      "Apple iPhone 7 Plus smartphone was launched in September 2016. The phone comes with a 5.50-inch touchscreen display with a resolution of 1080x1920 pixels at a pixel density of 401 pixels per inch (ppi). Apple iPhone 7 Plus is powered by a quad-core Apple A10 Fusion processor. It comes with 3GB of RAM. The Apple iPhone 7 Plus runs iOS 10 and is powered by a 2900mAh non-removable battery.As far as the cameras are concerned, the Apple iPhone 7 Plus on the rear packs 12-megapixel camera. It sports a 7-megapixel camera on the front for selfies.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Smashed Iphone",
+    img: "img/product-8.png",
+    price: 2,
+    company: "apple",
+    info:
+      "Smashed but with properly working condition ,Apple iPhone 6 32GB smartphone has a IPS LCD display. It measures 137.1 mm x 67 mm x 6.9 mm and weighs 129 grams. The screen has a resolution of 750 x 1334 pixels and 326 ppi pixel density. It has an aspect ratio of 16:9 and screen-to-body ratio of 66.14 %. On camera front, the buyers get a 1.2 MP f/2.2 Primary Camera(31 mm focal length) and on the rear, there's an 8 MP camera with features like Digital Zoom, Auto Flash, Digital image stabilization, Face detection, Simultaneous HD video and image recording, Touch to focus. It is backed by a 1810 mAh battery.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Google Pixel 3 smartphone has a OLED display. It measures 145.6 mm x 68.2 mm x 7.9 mm and weighs 148 grams. The screen has a resolution of 1080 x 2160 pixels and 439 ppi pixel density. It has an aspect ratio of 18:9 and screen-to-body ratio of 78.62 %. On camera front, the buyers get a 8 MP f/2.2, Wide Angle Primary Camera(28 mm focal length)8 MP, Wide Angle, Ultra-Wide Angle Camera and on the rear, there's an 12.2 MP camera with features like Digital Zoom, Auto Flash, Face detection, Touch to focus. It is backed by a 2915 mAh battery.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
